8681083948517edc4626a75fe04c349d70d493c2
sboyadzhiev <s.boyadzhiev@mail.bg>
2021-07-28 12:55:44 +0300

    End of secion 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Activity> Activities { get; set; }       
+
+        
+    }
+} 
